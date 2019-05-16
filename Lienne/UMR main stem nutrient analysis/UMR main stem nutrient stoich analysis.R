f27e2124ec77f89b0e3fcd59c55dc4456ef307c1
Lienne Sethna <lsethna@iu.edu>
2019-05-16 13:42:29 -0400

    Averages of UMR mainstem stoich

--- Lienne/UMR main stem nutrient analysis/UMR main stem nutrient stoich analysis.R
+++ Lienne/UMR main stem nutrient analysis/UMR main stem nutrient stoich analysis.R
@@ -18,6 +18,10 @@ plot(UMR_mainstem_nutrients$NSi_ratio, UMR_mainstem_nutrients$PSi_ratio)
 #calculate average N:Si and P:Si by day of year (DOY)
 library(lubridate)
 UMR_mainstem_nutrients$DOY = yday(UMR_mainstem_nutrients$DATE)
+UMR_mainstem_stoichavg = aggregate(UMR_mainstem_stoich, by=list(UMR_mainstem_stoich$DOY), FUN=mean)
+plot(UMR_mainstem_stoichavg$DOY, UMR_mainstem_nutrients$NSi_ratio, 
+     xlab="DOY", ylab="Average N:Si ratio", main="UMR mainstem sites 1-7",
+     cex=0.5)
 
 ##separate by sites
 UMR_mainstem_site1 = subset(UMR_mainstem_nutrients, UMR_mainstem_nutrients$FLDNUM==1)
@@ -29,3 +33,9 @@ UMR_mainstem_site6 = subset(UMR_mainstem_nutrients, UMR_mainstem_nutrients$FLDNU
 UMR_mainstem_site7 = subset(UMR_mainstem_nutrients, UMR_mainstem_nutrients$FLDNUM==7)
 
 UMR_site1_stoichavg = data.frame(aggregate(UMR_mainstem_site1, by=list(UMR_mainstem_site1$DOY), FUN=mean))
+plot(UMR_site1_stoichavg$DOY, UMR_site1_stoichavg$NSi, xlab="DOY", ylab="Average N:Si", main="UMR Site 1")
+plot(UMR_site1_stoichavg$DOY, UMR_site1_stoichavg$PSi, xlab="DOY", ylab="Average P:Si", main="UMR Site 1")
+
+UMR_site2_stoichavg = data.frame(aggregate(UMR_mainstem_site2, by=list(UMR_mainstem_site2$DOY), FUN=mean))
+plot(UMR_site2_stoichavg$DOY, UMR_site2_stoichavg$NSi, xlab="DOY", ylab="Average N:Si", main="UMR Site 2")
+
